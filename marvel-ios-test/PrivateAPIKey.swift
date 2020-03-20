7649e39e20c75927230b7d9bbfe48d8f9cf603ec
Zachary Esmaelzada <zackesmaelzada@gmail.com>
2020-03-20 18:40:57 -0400

    api key edits

---
+++ marvel-ios-test/PrivateAPIKey.swift
@@ -0,0 +1,14 @@
+//
+//  PrivateAPIKey.swift
+//  marvel-ios-test
+//
+//  Created by Zachary Esmaelzada on 3/19/20.
+//  Copyright © 2020 Zachary Esmaelzada. All rights reserved.
+//
+
+import Foundation
+
+struct MarvelAPIKey {
+    static let publicKey = ""
+    static let privateKey = ""
+}
